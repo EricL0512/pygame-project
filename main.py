3f55f8cee404797b028e9be3688354e3134a3602
Eric Lin <ericl160@nycstudents.net>
2024-05-13 15:23:24 -0400

    Started adding some sprites and working on the main menu

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1920, 1080))
 clock = pygame.time.Clock()
 running = True
 
@@ -9,10 +9,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_r]:  # This will be used to switch screens
+        running = False
+    if keys[pygame.K_e]:  # This will be used to access the menu. These can and will change later but this is a temp.
+        running = False
     screen.fill("blue")
-
-    # RENDER YOUR GAME HERE
+    pygame.display.update()
 
     clock.tick(60)
 
