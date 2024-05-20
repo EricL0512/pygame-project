ebd3d7c1abd82cfc87b92b7673c80fcf0d692871
Eric Lin <ericl160@nycstudents.net>
2024-05-20 15:26:43 -0400

    worked on the main menu screen and focused on how to modify images

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import pygame
-import sys
-import readline
 
 pygame.init()
 screen = pygame.display.set_mode((1920, 1080))
 clock = pygame.time.Clock()
-pygame.display.set_caption("test")
-running = True
-test = pygame.image.load("start.png")
+pygame.display.set_caption("start_button")
 
+start_button = pygame.image.load("start.png")
+start_button = pygame.transform.scale(start_button, (start_button.get_width() * 5, start_button.get_height() * 5))
+menu_background = pygame.image.load("menu_background.jpg")
+#menu_background = pygame.transform.scale()
+
+running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -18,7 +20,8 @@ while running:
         running = False
     if keys[pygame.K_e]:  # This will be used to access the menu. These can and will change later but this is a temp.
         running = False
-    screen.fill("blue")
+    screen.blit(menu_background, (0, 0))
+    screen.blit(start_button, ((screen.get_width()/2) - (start_button.get_width()/2), (screen.get_height()/2) - (start_button.get_height())/2))
     pygame.display.update()
     clock.tick(60)
 
