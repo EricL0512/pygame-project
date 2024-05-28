b39845aa0b5ed19f84833a401836dfac9121f8a8
Eric Lin <ericl160@nycstudents.net>
2024-05-28 15:12:03 -0400

    reformated the files. hopefully will make it easier for me in the future.

--- menu.py
+++ menu.py
@@ -1,1 +1,48 @@
+import pygame
+import sys
+
+
 class Menu:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1920, 1080))
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption("start_button")
+
+        self.start_button = pygame.image.load("start.png")
+        self.start_button = pygame.transform.scale(self.start_button,
+                                                   (self.start_button.get_width() * 5,
+                                                    self.start_button.get_height() * 5))
+        self.menu_background = pygame.image.load("menu_background.jpg")
+        # menu_background = pygame.transform.scale()
+
+        self.game_background = pygame.image.load("background test.jpg")
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    keys = pygame.key.get_pressed()
+                    if keys[pygame.K_r]:  # This will be used to switch screens
+                        pass
+                    if keys[pygame.K_e]:
+                        pass
+            self.screen.blit(self.menu_background, (0, 0))
+            self.screen.blit(self.start_button, ((self.screen.get_width() / 2) - (self.start_button.get_width() / 2),
+                                                 (self.screen.get_height() / 2) - (self.start_button.get_height()) / 2))
+            self.screen.blit(self.start_button,
+                             ((self.screen.get_width() / 2) - (self.start_button.get_width() / 2),
+                              # CHANGE TO A DIFFERENT IMAGE LATER
+                              (self.screen.get_height() / 2) - (
+                                  self.start_button.get_height()) / 2 + self.start_button.get_height() + 50))
+            self.screen.blit(self.start_button, ((self.screen.get_width() / 2) - (self.start_button.get_width() / 2),
+                                                 (self.screen.get_height() / 2) - (
+                                                     self.start_button.get_height()) / 2 + self.start_button.get_height() * 2 + 100))
+            self.screen.blit(self.start_button, ((self.screen.get_width() / 2) - (self.start_button.get_width() / 2),
+                                                 (self.screen.get_height() / 2) - (
+                                                     self.start_button.get_height()) / 2 - self.start_button.get_height() - 50))
+            self.clock.tick(60)
+            pygame.display.update()
+
+
+Menu().run()
