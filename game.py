b39899f35a93196c20e648f7ed05006da9e6ac99
Eric Lin <ericl160@nycstudents.net>
2024-05-29 15:32:06 -0400

    starting working on game class and added more functions to organize code

--- game.py
+++ game.py
@@ -1,5 +1,27 @@
 import sys
 import pygame
 
+
 class Game:
     def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1920, 1080))
+        self.clock = pygame.time.Clock()
+        self.game_background = pygame.image.load("background test.jpg")
+        self.game_background = pygame.transform.scale_by(self.game_background, 10)
+
+    def run(self):
+        running = True
+        while running:
+            x, y = pygame.mouse.get_pos()
+            self.draw()
+            self.clock.tick(60)
+            pygame.display.update()
+
+    def draw(self):
+        self.screen.blit(self.game_background, (0, 0))
+
+    def iterate(self, x):
+        if x == 0:
+            return 0
+        return 1 + self.iterate(x - 1)
--- menu.py
+++ menu.py
@@ -1,6 +1,8 @@
 import pygame
 import sys
 
+from game import Game
+
 
 class Menu:
     def __init__(self):
@@ -14,34 +16,40 @@ class Menu:
                                                    (self.start_button.get_width() * 5,
                                                     self.start_button.get_height() * 5))
         self.menu_background = pygame.image.load("menu_background.jpg")
-        # menu_background = pygame.transform.scale()
-
-        self.game_background = pygame.image.load("background test.jpg")
 
     def run(self):
-        while True:
+        running = True
+        while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
+                    pass
+                if event.type == pygame.KEYDOWN:
                     keys = pygame.key.get_pressed()
                     if keys[pygame.K_r]:  # This will be used to switch screens
-                        pass
+                        print('bruh')
+                        Game().run()
+                        running = False
                     if keys[pygame.K_e]:
                         pass
-            self.screen.blit(self.menu_background, (0, 0))
-            self.screen.blit(self.start_button, ((self.screen.get_width() / 2) - (self.start_button.get_width() / 2),
-                                                 (self.screen.get_height() / 2) - (self.start_button.get_height()) / 2))
-            self.screen.blit(self.start_button,
-                             ((self.screen.get_width() / 2) - (self.start_button.get_width() / 2),
-                              # CHANGE TO A DIFFERENT IMAGE LATER
-                              (self.screen.get_height() / 2) - (
-                                  self.start_button.get_height()) / 2 + self.start_button.get_height() + 50))
-            self.screen.blit(self.start_button, ((self.screen.get_width() / 2) - (self.start_button.get_width() / 2),
-                                                 (self.screen.get_height() / 2) - (
-                                                     self.start_button.get_height()) / 2 + self.start_button.get_height() * 2 + 100))
-            self.screen.blit(self.start_button, ((self.screen.get_width() / 2) - (self.start_button.get_width() / 2),
-                                                 (self.screen.get_height() / 2) - (
-                                                     self.start_button.get_height()) / 2 - self.start_button.get_height() - 50))
+            self.draw()
             self.clock.tick(60)
             pygame.display.update()
+
+    def draw(self):
+        self.screen.blit(self.menu_background, (0, 0))
+        self.screen.blit(self.start_button, ((self.screen.get_width() / 2) - (self.start_button.get_width() / 2),
+                                             (self.screen.get_height() / 2) - (self.start_button.get_height()) / 2))
+        self.screen.blit(self.start_button,
+                         ((self.screen.get_width() / 2) - (self.start_button.get_width() / 2),
+                          # CHANGE TO A DIFFERENT IMAGE LATER
+                          (self.screen.get_height() / 2) - (
+                              self.start_button.get_height()) / 2 + self.start_button.get_height() + 50))
+        self.screen.blit(self.start_button, ((self.screen.get_width() / 2) - (self.start_button.get_width() / 2),
+                                             (self.screen.get_height() / 2) - (
+                                                 self.start_button.get_height()) / 2 + self.start_button.get_height() * 2 + 100))
+        self.screen.blit(self.start_button, ((self.screen.get_width() / 2) - (self.start_button.get_width() / 2),
+                                             (self.screen.get_height() / 2) - (
+                                                 self.start_button.get_height()) / 2 - self.start_button.get_height() - 50))
+
 
 Menu().run()
